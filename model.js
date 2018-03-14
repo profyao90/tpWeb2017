edd1d64e906aeaf06f9896ec8c2cd1dcacbdad65
profyao90 <aubinya90@gmail.com>
2018-03-15 00:01:54 +0100

    fonction Rectangle

--- model.js
+++ model.js
@@ -20,3 +20,29 @@ function  Forme(epaisseur,color){
 		return this.epaisseur;
 	}.bind(this);
 }
+function Rectangle(coordRECX, coordRECY, largeur, hauteur,epaisseur,color){
+    Forme.call(this, epaisseur,color);
+    this.coordRECX=coordRECX 
+    this.coordRECY=coordRECY 
+    this.largeur=largeur
+    this.hauteur=hauteur
+    this.epaisseur=epaisseur
+    this.color=color
+
+        //Fonction qui retourne la coordonnée de X
+    this.getInitX = function(){
+        return this.coordRECX;
+    }
+    //Fonction qui retourne la coordonnée de Y
+    this.getInitY = function(){
+        return this.coordRECY;
+    }
+    //Fonction qui retourne la coordonnée de X final
+    this.getFinalX = function(){
+        return (this.coordRECX + this.largeur);
+    }
+    //Fonction qui retourne la coordonnée de Y final
+    this.getFinalY = function(){
+        return (this.coordRECY + this.hauteur);
+    }
+} Rectangle.prototype = new Forme();
