21aceb7594145218b9068efe7ca2013c96eb0a37
profyao90 <aubinya90@gmail.com>
2018-03-14 23:58:17 +0100

    class Forme

--- model.js
+++ model.js
@@ -6,3 +6,17 @@
 function Drawing() {
     this.monTableau = new Array();
 };
+
+function  Forme(epaisseur,color){
+    this.color = color;
+    this.epaisseur=epaisseur;
+     //Fonction paint Q7
+
+    this.getColor = function(){
+		return this.color;
+	}.bind(this);
+	
+	this.getEpaisseur = function(){
+		return this.epaisseur;
+	}.bind(this);
+}
